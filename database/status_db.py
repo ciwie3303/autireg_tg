2e981b67b26344fd0f0366549268e151d7109c0c
UNKNWNCLOWNGHL <58516721+ciwie3303@users.noreply.github.com>
2024-05-01 17:42:45 +0400

    Update status_db.py

--- database/status_db.py
+++ database/status_db.py
@@ -1,17 +1,21 @@
 from peewee import *
 
 
-db = SqliteDatabase('database/status.db')
+db = SqliteDatabase('database/database.db')
 
 
 class Settings(Model):
     ID = IntegerField(primary_key=True)
     status = IntegerField(default=0)
     API_token = CharField()
+    Country = IntegerField(default=0)
+    Service = CharField()
+    MaxPrice = IntegerField()
 
     class Meta:
         database = db
 
 
 if __name__ == '__main__':
+
     db.connect()
