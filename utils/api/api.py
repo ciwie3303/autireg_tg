fa21a381533b86e71182da7a5e4841bcf8309084
UNKNWNCLOWNGHL <58516721+ciwie3303@users.noreply.github.com>
2024-05-01 11:47:55 +0400

    Update api.py

--- utils/api/api.py
+++ utils/api/api.py
@@ -1,44 +1,51 @@
-import requests
+import aiohttp
+import asyncio
 import json
 import time
 
 API_BASE_URL = "https://api.sms-activate.org/stubs/handler_api.php"
 
+async def get_countries(api_key: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{API_BASE_URL}?api_key={api_key}&action=getCountries") as response:
+            response_text = await response.text()
+            try:
+                countries_data = json.loads(response_text)
+            except json.JSONDecodeError:
+                print("Ошибка декодирования JSON. Неверный формат ответа.")
+                return None
 
-def get_countries(api_key: str):
-    response = requests.post(f"{API_BASE_URL}?api_key={api_key}&action=getCountries")
-    countries_data = json.loads(response.text)
-    
-    countries_rus = {}
-    print("Список стран:")
-    for country_id, country_info in countries_data.items():
-        if 'rus' in country_info:
-            country_name = country_info['rus']
-            countries_rus[country_id] = country_name
-            print(country_name)
-    
-    return countries_rus
-
-
-def get_number(api_key: str, service: str, max_price: int, country: int):
-    response = requests.post(f"{API_BASE_URL}?api_key={api_key}&action=getNumber&service={service}&country={country}&maxPrice={max_price}")
-    result = response.text.split(":")
-    print(result)
-    if result[0] == "ACCESS_NUMBER":
-        return result[1] 
-    else:
-        print("Не удалось получить номер.")
-        return None
+            countries_rus = {}
+            print("Список стран:")
+            for country_id, country_info in countries_data.items():
+                if 'rus' in country_info:
+                    country_name = country_info['rus']
+                    countries_rus[country_id] = (country_name, country_id) 
+                    print(f"ID: {country_id} | {country_name}")  
+            
+            return countries_rus
 
 
-def get_status(api_key: str, number_id: int):
-    response = requests.post(f"{API_BASE_URL}?api_key={api_key}&action=getStatus&id={number_id}")
-    return response.text
+async def get_number(api_key: str, service: str, max_price: int, country: int):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{API_BASE_URL}?api_key={api_key}&action=getNumber&service={service}&country={country}&maxPrice={max_price}") as response:
+            result = await response.text()
+            print(result)
+            result = result.split(":")
+            if result[0] == "ACCESS_NUMBER":
+                return result[1]  
+            else:
+                print("Не удалось получить номер.")
+                return None
 
+async def get_status(api_key: str, number_id: int):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{API_BASE_URL}?api_key={api_key}&action=getStatus&id={number_id}") as response:
+            return await response.text()
 
-def main():
+async def main():
     api_key = '2016dfA7389e032ebA02707cceb581cc'
-    countries = get_countries(api_key)
+    countries = await get_countries(api_key)
     if not countries:
         print("Не удалось получить список стран. Проверьте ваш API ключ.")
         return
@@ -47,22 +54,18 @@ def main():
     service = 'tg'
     max_price = '18'
 
-    number_id = get_number(api_key, service, max_price, country_id)
+    number_id = await get_number(api_key, service, max_price, country_id)
     if not number_id:
         return
 
     print("Номер успешно получен. ID номера:", number_id)
 
     while True:
-        status = get_status(api_key, number_id)
+        status = await get_status(api_key, number_id)
         print("Статус номера:", status)
         if status != "STATUS_WAIT_CODE":
             break
-        time.sleep(30)
-
+        await asyncio.sleep(30)
 
 if __name__ == "__main__":
-    main()
-
-
-
+    asyncio.run(main())
