05db86b9a451ad64e2f7f1f6f979f3e7737cccb0
UNKNWNCLOWNGHL <58516721+ciwie3303@users.noreply.github.com>
2024-05-01 17:42:23 +0400

    Update back.py

--- handlers/back.py
+++ handlers/back.py
@@ -9,5 +9,7 @@ from aiogram.fsm.context import FSMContext
 @dp.callback_query(F.data == 'back', StateFilter('*'))
 async def start_handler(call: CallbackQuery, state: FSMContext):
     await state.clear()
-    await call.message.edit_text(f'Привет, <b>{call.from_user.full_name}!</b> Это панель управления <b>проектом.</b>',
-                              reply_markup=await start_keyboard())
+    await call.message.edit_text(f'''
+Привет, <b>{call.from_user.full_name}!</b> 
+
+Это панель управления <b>проектом.</b>''', reply_markup=await start_keyboard())
